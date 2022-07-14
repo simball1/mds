1e8f17173dc15b1aa2a219671361bced533bcd8d
simball1 <simball1@kakao.com>
2022-07-14 16:40:45 +0900

    Feat: 영화 등록기능 - 영화 추가 기능(영화제목, 감독이름, 배우이름, 시놉시스) - 영화 배우 엔티티 추가 - 영화 상세페이지 표시

--- src/main/java/com/mysite/mds/movie/Movie.java
+++ src/main/java/com/mysite/mds/movie/Movie.java
@@ -1,7 +1,7 @@
 package com.mysite.mds.movie;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -9,9 +9,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.mysite.mds.actor.Actor;
 import com.mysite.mds.comment.Comment;
 import com.mysite.mds.director.Director;
 
@@ -29,17 +31,21 @@ public class Movie {
 
 	@Column(length = 100)
 	private String title;
-
-	@Column
-	private LocalDateTime releaseDate;
-
+	
+	@ManyToOne
+	private Director director;
+	
+	@ManyToMany
+	Set<Actor> actor;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
 	@Column
 	private Double avgStarScore;
 
 	@OneToMany(mappedBy = "movie", cascade = CascadeType.REMOVE)
 	private List<Comment> commentList;
 
-	@ManyToOne
-	private Director director;
-
+	
 }
